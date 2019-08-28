640bdbd282989e0966d3a4e83836b3f1102b607b
hgreen-akl <h.green@auckland.ac.nz>
2019-08-29 10:32:11 +1200

    Add plot_by_distance()

---
+++ R/plot_by_distance.R
@@ -0,0 +1,14 @@
+#' Plots raw trimmed data by time for visual data check
+#'
+#'
+#'
+#'
+#' @param x A dataframe of the drivesim parameters
+#'
+#' @return A faceted figure of all variables recorded over distance
+
+plot_by_distance <- function(x){
+
+    data_to_plot <- x %>% gather(Longitudinal_Veloc:Road_Curve, key = "Variable", value = "value")
+    ggplot(data_to_plot, aes(Total_dist, value)) + geom_line() + facet_wrap(~ Variable, scales = "free")
+}
