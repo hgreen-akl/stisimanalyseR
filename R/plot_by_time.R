917d6502547a8bf652d91061e0db4a3a306c2b9c
hgreen-akl <h.green@auckland.ac.nz>
2019-08-29 13:03:10 +1200

    Add plot_by_time() and fix plot_by_dist()

--- R/plot_by_time.R
+++ R/plot_by_time.R
@@ -12,11 +12,12 @@
 #' @export
 #'
 
-if(getRversion() >= "2.15.1")  utils::globalVariables(c("Longitudinal_Veloc", "Road_Curve", "Total_dist", "Metric"))
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("Longitudinal_Veloc", "Road_Curve", "Elapsed_Time", "Metric"))
 
-plot_by_distance <- function(x){
-
+plot_by_time <- function(x) {
     data_to_plot <- x %>% gather(Longitudinal_Veloc:Road_Curve, key = "Variable", value = "Metric")
 
-    ggplot(data_to_plot, aes(Total_dist, Metric)) + geom_line() + facet_wrap(~ Variable, scales = "free")
+    ggplot(data_to_plot, aes(Elapsed_Time, Metric)) +
+        geom_line() +
+        facet_wrap(~ Variable, scales = "free")
 }
